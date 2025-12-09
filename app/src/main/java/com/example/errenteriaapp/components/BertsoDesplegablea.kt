a0f56d01003104fa73f1db754e8b0e7354ba0b73
Belatz Arnaiz <b.arnaiz@gmail.com>
2025-12-09 10:55:46 +0100

    Add BertsoDesplegablea component and integrate into BertsoJolasaScreen

--- app/src/main/java/com/example/errenteriaapp/components/BertsoDesplegablea.kt
+++ app/src/main/java/com/example/errenteriaapp/components/BertsoDesplegablea.kt
@@ -1,2 +1,89 @@
 package com.example.errenteriaapp.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BertsoDesplegablea(a: String, b: String, c: String, ColorBox: Long) {
+    Box (
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .background(Color(ColorBox), RoundedCornerShape(24.dp)),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp, vertical = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(horizontal = 8.dp)
+                        .border(
+                            width = 0.01.dp,
+                            color = Color.Black,
+                            shape = RoundedCornerShape(24.dp)
+                        )
+                        .background(Color.White, RoundedCornerShape(20.dp)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(a)
+
+                }
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(horizontal = 8.dp)
+                        .border(
+                            width = 0.01.dp,
+                            color = Color.Black,
+                            shape = RoundedCornerShape(24.dp)
+                        )
+                        .background(Color.White, RoundedCornerShape(20.dp)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(b)
+
+                }
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(horizontal = 8.dp)
+                        .border(
+                            width = 0.01.dp,
+                            color = Color.Black,
+                            shape = RoundedCornerShape(24.dp)
+                        )
+                        .background(Color.White, RoundedCornerShape(20.dp)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(c)
+
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/errenteriaapp/navigation/screens/BertsoJolasa.kt
+++ app/src/main/java/com/example/errenteriaapp/navigation/screens/BertsoJolasa.kt
@@ -1,25 +1,13 @@
 package com.example.errenteriaapp.navigation.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.errenteriaapp.components.BertsoDesplegablea
 
 @Composable
 fun BertsoJolasaScreen(
@@ -27,150 +15,123 @@ fun BertsoJolasaScreen(
 
     LazyColumn {
         item {
-            Box (
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 16.dp, vertical = 8.dp)
-                    .background(Color(0xFFFFC1C1), RoundedCornerShape(24.dp)),
-                contentAlignment = Alignment.Center
-            ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 24.dp, vertical = 16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceEvenly,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Box(
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(horizontal = 8.dp)
-                                .border(
-                                    width = 0.01.dp,
-                                    color = Color.Black,
-                                    shape = RoundedCornerShape(24.dp)
-                                )
-                                .background(Color.White, RoundedCornerShape(20.dp)),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text("a" )
-
-                        }
-                        Box(
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(horizontal = 8.dp)
-                                .border(
-                                    width = 0.01.dp,
-                                    color = Color.Black,
-                                    shape = RoundedCornerShape(24.dp)
-                                )
-                                .background(Color.White, RoundedCornerShape(20.dp)),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text("b" )
-
-                        }
-                        Box(
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(horizontal = 8.dp)
-                                .border(
-                                    width = 0.01.dp,
-                                    color = Color.Black,
-                                    shape = RoundedCornerShape(24.dp)
-                                )
-                                .background(Color.White, RoundedCornerShape(20.dp)),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text("c" )
-
-                        }
-                    }
-                }
-
-            }
+            BertsoDesplegablea(
+                a = "a",
+                b = "b",
+                c = "c",
+                ColorBox = 0xFFFFC1C1
+            )
         }
         item {
-            Box (
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 16.dp, vertical = 8.dp)
-                    .background(Color(0xFFBDFFC0), RoundedCornerShape(24.dp)),
-                contentAlignment = Alignment.Center
-            ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 24.dp, vertical = 16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceEvenly,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Box(
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(horizontal = 8.dp)
-                                .border(
-                                    width = 0.01.dp,
-                                    color = Color.Black,
-                                    shape = RoundedCornerShape(24.dp)
-                                )
-                                .background(Color.White, RoundedCornerShape(20.dp)),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text("a" )
-
-                        }
-                        Box(
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(horizontal = 8.dp)
-                                .border(
-                                    width = 0.01.dp,
-                                    color = Color.Black,
-                                    shape = RoundedCornerShape(24.dp)
-                                )
-                                .background(Color.White, RoundedCornerShape(20.dp)),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text("b" )
-
-                        }
-                        Box(
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(horizontal = 8.dp)
-                                .border(
-                                    width = 0.01.dp,
-                                    color = Color.Black,
-                                    shape = RoundedCornerShape(24.dp)
-                                )
-                                .background(Color.White, RoundedCornerShape(20.dp)),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text("c" )
-
-                        }
-                    }
-                }
-
-            }
+            BertsoDesplegablea(
+                a = "a",
+                b = "b",
+                c = "c",
+                ColorBox = 0xFFBDFFC0
+            )
         }
-
+        item {
+            BertsoDesplegablea(
+                a = "a",
+                b = "b",
+                c = "c",
+                ColorBox = 0xFFBDCEFF
+            )
+        }
+        item {
+            BertsoDesplegablea(
+                a = "a",
+                b = "b",
+                c = "c",
+                ColorBox = 0xFFC5904E
+            )
+        }
+        item {
+            BertsoDesplegablea(
+                a = "a",
+                b = "b",
+                c = "c",
+                ColorBox = 0xFFB6B6B6
+            )
+        }
+        item {
+            BertsoDesplegablea(
+                a = "a",
+                b = "b",
+                c = "c",
+                ColorBox = 0xFFE0FF6F
+            )
+        }
+        item {
+            BertsoDesplegablea(
+                a = "a",
+                b = "b",
+                c = "c",
+                ColorBox = 0xFFFFE4C4
+            )
+        }
+        item {
+            BertsoDesplegablea(
+                a = "a",
+                b = "b",
+                c = "c",
+                ColorBox = 0xFFFFD1DC
+            )
+        }
+        item {
+            BertsoDesplegablea(
+                a = "a",
+                b = "b",
+                c = "c",
+                ColorBox = 0xFFE0BBE4
+            )
+        }
+        item {
+            BertsoDesplegablea(
+                a = "a",
+                b = "b",
+                c = "c",
+                ColorBox = 0xFF6FFFBC
+            )
+        }
+        item {
+            BertsoDesplegablea(
+                a = "a",
+                b = "b",
+                c = "c",
+                ColorBox = 0xFFCCE2CB
+            )
+        }
+        item {
+            BertsoDesplegablea(
+                a = "a",
+                b = "b",
+                c = "c",
+                ColorBox = 0xFFB5EAD7
+            )
+        }
+        item {
+            BertsoDesplegablea(
+                a = "a",
+                b = "b",
+                c = "c",
+                ColorBox = 0xFFA7FF6F
+            )
+        }
+        item {
+            BertsoDesplegablea(
+                a = "a",
+                b = "b",
+                c = "c",
+                ColorBox = 0xFDF6BCFF
+            )
+        }
     }
-
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun BertsoJolasaPreview() {
-    BertsoJolasaScreen(rememberNavController())
+    BertsoJolasaScreen(navController = rememberNavController()
+    )
 }
