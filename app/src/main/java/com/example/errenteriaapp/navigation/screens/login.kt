5cc2ae02bf2f98db22702a4fa197cfffb87c7bed
AitorCuesta <a.cuestasanchez@ikasle.eus>
2025-12-12 14:20:05 +0100

    Refactor login screen layout; enhance character item responsiveness and improve image handling

--- app/src/main/java/com/example/errenteriaapp/navigation/screens/login.kt
+++ app/src/main/java/com/example/errenteriaapp/navigation/screens/login.kt
@@ -1,6 +1,5 @@
 package com.example.errenteriaapp.navigation.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
@@ -8,10 +7,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.errenteriaapp.R
 import com.example.errenteriaapp.components.*
